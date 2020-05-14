ab31dbf5e7d748a5fbcf82f604c77e148769bcf7
Toroshhin <https://github.com/Toroshhin>
2020-05-14 19:49:42 +0300

    fixed bugs

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ function moveUp() {
 
 // Создание блоков
 var pipe = [];
-
 pipe[0] = {
   x: cvs.width,
   y: 0
@@ -47,13 +46,10 @@ var grav = 1.5;
 
 function draw() {
   ctx.drawImage(bg, 0, 0);
-
   for (var i = 0; i < pipe.length; i++) {
     ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
     ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-
     pipe[i].x--;
-
     if (pipe[i].x == 125) {
       pipe.push({
         x: cvs.width,
@@ -66,7 +62,11 @@ function draw() {
       xPos <= pipe[i].x + pipeUp.width &&
       (yPos <= pipe[i].y + pipeUp.height ||
         yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-      location.reload(); // Перезагрузка страницы
+      // Перезагрузка страницы
+      cancelAnimationFrame(draw);
+      //ctx.clearRect(0, 0, canvas.width, canvas.height);
+      return;
+
     }
 
     if (pipe[i].x == 5) {
@@ -77,13 +77,10 @@ function draw() {
 
   ctx.drawImage(fg, 0, cvs.height - fg.height);
   ctx.drawImage(bird, xPos, yPos);
-
   yPos += grav;
-
   ctx.fillStyle = "#000";
   ctx.font = "24px Verdana";
   ctx.fillText("Счет: " + score, 10, cvs.height - 20);
-
   requestAnimationFrame(draw);
 }
 
